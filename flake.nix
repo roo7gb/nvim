00fb6bcb7de3202d113d610908b625bf4c2effe7
roo7gb <miasma1007@gmail.com>
2026-07-19 22:25:01 -0400

    Fix colors

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    packages.${system}.gb-nvim =
+    packages.${system}.default =
       (nvf.lib.neovimConfiguration {
         inherit pkgs;
         modules = [
@@ -29,7 +29,7 @@
                 };
 
                 statusline.lualine.enable = true;
-                autocomplete.blink_cmp.enable = true;
+                autocomplete.blink-cmp.enable = true;
 
                 telescope = {
                   enable = true;
@@ -79,22 +79,22 @@
                   transparent = true;
 
                   base16-colors = {
-                    base00 = "#161616";
-                    base01 = "#d6717b";
-                    base02 = "#8ed671";
-                    base03 = "#d6b471";
-                    base04 = "#3395b2";
-                    base05 = "#8c7094";
-                    base06 = "#6965a6";
-                    base07 = "#dadada";
-                    base08 = "#606060";
-                    base09 = "#d6404f";
-                    base0A = "#72d64b";
-                    base0B = "#d6a84b";
-                    base0C = "#29a6cc";
-                    base0D = "#896094";
-                    base0E = "#5f59b3";
-                    base0F = "#f8f8f8";
+                    base00 = "#151515";
+                    base01 = "#161616";
+                    base02 = "#242424";
+                    base03 = "#5c5c5c";
+                    base04 = "#6a6a6a";
+                    base05 = "#b3aea2";
+                    base06 = "#dadada";
+                    base07 = "#4e4e4e";
+                    base08 = "#d6717b";
+                    base09 = "#d68571";
+                    base0A = "#8c7094";
+                    base0B = "#8ed671";
+                    base0C = "#962b36";
+                    base0D = "#6965a6";
+                    base0E = "#d6b471";
+                    base0F = "#29a6cc";
                   };
                 };
 
@@ -110,6 +110,7 @@
                   LineNr = {
                     bg = "NONE";
                     ctermbg = "NONE";
+                    fg = "#5c5c5c";
                   };
                   SignColumn = {
                     bg = "NONE";
@@ -118,6 +119,7 @@
                   CursorLineNr = {
                     bg = "NONE";
                     ctermbg = "NONE";
+                    fg = "#5c5c5c";
                   };
                 };
               };
